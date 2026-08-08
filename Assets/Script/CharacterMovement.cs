4e4bf0b9b0a8661c2caaa06b0a078d2f91381bac
caomanhcuong0912-maker <broisunreal123@gmail.com>
2026-08-08 16:18:13 +0700

    UI

---
+++ Assets/Script/CharacterMovement.cs
@@ -0,0 +1,30 @@
+
+using UnityEngine;
+
+
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float jumpPower = 2f;
+    [SerializeField] private float speedForce = 2f;
+    [SerializeField] private LayerMask GroundLayer;
+    [SerializeField] private Transform FeetPos;
+    [SerializeField] private float GroundDistance = 0.2f;
+    private bool isGrounded;
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(FeetPos.position, GroundDistance, GroundLayer);
+        Debug.Log("isGrounded: " + isGrounded);
+
+        float moveX =Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(moveX * speedForce, rb.linearVelocity.y);
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+        }
+    }
+
+    
+   
+}
