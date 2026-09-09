1b8cb6bee6318376c42c9a73dd304c6c682d8ce4
caomanhcuong0912-maker <broisunreal123@gmail.com>
2026-09-09 14:35:58 +0700

    aaa

--- Assets/Script/ZombieHitbox.cs
+++ Assets/Script/ZombieHitbox.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 
-public class ZombieKHitbox : MonoBehaviour
+public class ZombieHitbox : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Destroy(other.gameObject);
+            Destroy(gameObject);
             GameManagerMap2.Instance.showLoseMenu();
         }
     }
