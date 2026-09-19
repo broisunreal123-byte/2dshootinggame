f6b41a6b483bccb4b5dd05c54ed1ebebb3580bd6
caomanhcuong0912-maker <broisunreal123@gmail.com>
2026-09-19 17:02:06 +0700

    flying enemy

--- Assets/Script/ZombieMovement.cs
+++ Assets/Script/ZombieMovement.cs
@@ -9,6 +9,7 @@ public class ZombieMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     void Update()
     {
+        if (player == null) return;
         float direction = Math.Sign(player.position.x - transform.position.x);
         rb.linearVelocity = new Vector2(direction * zombieSpeed, rb.linearVelocity.y);
     }
