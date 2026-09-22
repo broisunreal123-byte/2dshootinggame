028995de6e965c4449f77cb40f82fe7cb695ec4c
caomanhcuong0912-maker <broisunreal123@gmail.com>
2026-09-22 14:27:44 +0700

    yes

--- Assets/Script/FlyingEnemy/FlyingEnemy.cs
+++ Assets/Script/FlyingEnemy/FlyingEnemy.cs
@@ -1,14 +1,28 @@
 
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FlyingEnemy : MonoBehaviour
 {
     private int direction = 1;
     [SerializeField] private float speed =  2f;
+    [SerializeField] private GameObject fireball;
+    [SerializeField] private Transform fireballspawnpoint;
+    [SerializeField] private float spawnTime = 1f;
+    void Start()
+    {
+        InvokeRepeating(nameof(spawnFireball), spawnTime, spawnTime - 0.5f);
+    }
     void Update()
     {
         transform.Translate(Vector2.right * direction * speed * Time.deltaTime,Space.World);
+        
     }
+    private void spawnFireball()
+    {
+        Instantiate(fireball, fireballspawnpoint.position,fireballspawnpoint.rotation);
+    }
+    
     private void OnTriggerEnter2D (Collider2D other)
     {
         if (other.CompareTag("turningpoint"))
